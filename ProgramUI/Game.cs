0a0d8622e6905d4ead961167b0a0dc7d8f2c1813
Isaiah Schmidt <isiahdschmidt@outlook.com>
2023-11-16 21:15:11 -0500

    level manager

--- ProgramUI/Game.cs
+++ ProgramUI/Game.cs
@@ -1,30 +1,34 @@
 using ProgramUI.Levels.Rooms;
 using ProgramUI.Entites.Enemies;
+using ProgramUI.Levels;
+using ProgramUI.Entites;
 namespace ProgramUI;
 
 
 public class Game
 {
     // List of Levels
+    private LevelManager _levelManager;
+
     // Player
+    private Player _player;
+
     // Handle Input -> from the User in some way.
 
     // Welcome(Start) Screen
     // You Lose/Win Screens
     // Taken Damage Screen -> Player Specific?  
 
-    Room testing = new Room(RoomType.Up, "Testing 101", new List<Enemy>());
+    // Room testing = new Room(RoomType.Up, "Testing 101", new List<Enemy>());
 
     public Game() {
-        
+        _player = new Player("Player", 10, 2, 0, 0);
     }
+
 
     public void Run() {
         //Get the Room the Player
         // Level.Rooms[player.Y][player.X].Render();
-        testing.Render();
-        
 
-        
     }
 }
--- ProgramUI/Levels/LevelManager.cs
+++ ProgramUI/Levels/LevelManager.cs
@@ -1,14 +1,46 @@
+using ProgramUI.Levels.Rooms;
+
 namespace ProgramUI.Levels;
 
 public class LevelManager
 {
     // Hold a List of levels
+    public List<Level> Levels {get; private set; }
+
+    public Level CurrentLevel {get; private set; }
+
+    public LevelManager(List<Level> levels){
+        Levels = levels;
+         int newLevelNumber = 1;
+        foreach (Level level in Levels)
+        {
+            if (level.LevelNumber == newLevelNumber)
+            {
+                CurrentLevel = level;
+                break;
+            }
+        }
+    }
 
     //Currently Level
     
     //Method to Change Level
+    public void ChangeLevel(){
+        int newLevelNumber = CurrentLevel.LevelNumber + 1;
+        foreach (Level level in Levels)
+        {
+            if (level.LevelNumber == newLevelNumber)
+            {
+                CurrentLevel = level;
+                break;
+            }
+        }
+    }
 
-    //Method to get currenl level list of rooms
+    //Method to get current level list of rooms
+    public List<List<Room>> GetRooms(){
+        return CurrentLevel.Rooms;
+    }
 
     // TODO: A way to Programtically Build Levels from a file. 
 }
